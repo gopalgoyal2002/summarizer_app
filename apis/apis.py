8bc18c3fd5617147331751d2b87b37ad7b942b17
Gopal Goyal <79508610+gopalgoyal2002@users.noreply.github.com>
2022-08-09 00:47:37 +0530

    Update apis.py

--- apis/apis.py
+++ apis/apis.py
@@ -3,6 +3,11 @@ from huggingface import summrizer
 from request import Summaryrequest
 router = APIRouter()
 
+@router.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 @router.post("/summarizer")
 async def summarizer(request:Summaryrequest):
     return {"summarizer":summrizer(text=request.text,minlen=request.minL,maxlen=request.maxL)}
